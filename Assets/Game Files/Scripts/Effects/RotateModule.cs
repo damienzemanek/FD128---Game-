07a3a9a0e01d9b017830aaa830c0bd7f485f64b8
Emil Z <damienzemanek@lcad.edu>
2025-12-05 19:16:19 -0800

    prefab adjusments

--- Assets/Game Files/Scripts/Effects/RotateModule.cs
+++ Assets/Game Files/Scripts/Effects/RotateModule.cs
@@ -1,24 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static RotateExtension;
+using Random = UnityEngine.Random;
 
 public class RotateModule : MonoBehaviour
 {
     public ConstantRotate ConstantRotateModule;
+    public RandomRotation RandomRotationModule;
+
+    private void Start()
+    {
+        if (RandomRotationModule.active && RandomRotationModule.rotateOnSpawn) RandomRotationModule.Rotate();
+    }
 
     private void FixedUpdate()
     {
-        if(ConstantRotateModule.active)
+        if (ConstantRotateModule.active) ConstantRotateModule.Rotate();
     }
 }
 
 public static class RotateExtension
 {
 
+    [Serializable]
     public struct ConstantRotate
     {
         [SerializeField] public bool active;
         [SerializeField] Vector3 rotation;
+        [SerializeField] Transform transform;
+
+        public void Rotate() => transform.Rotate(rotation); 
+    }
+
+    [Serializable]
+    public struct RandomRotation
+    {
+        [SerializeField] public bool active;
+        public bool x, y, z;
+        [SerializeField] Transform transform;
+
+        public bool rotateOnSpawn;
+
+        public void Rotate()
+        {
+            Vector3 r = transform.eulerAngles;
+
+            if(x) r.x = Random.Range(0, 360);
+            if(y) r.y = Random.Range(0, 360);
+            if(z) r.z = Random.Range(0, 360);
+
+            transform.rotation = Quaternion.Euler(r);
+        }
+
     }
 
 }
