39b161a25ebdb38665fa8425d3bd1fbe64ec2fb7
Emil Z <istarnightstudios@gmail.com>
2025-12-12 01:10:32 -0800

    audio

---
+++ Assets/Game Files/Scripts/SFX/Ambience.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extensions;
+using static Extensions.AudioEX;
+
+public class Ambience : MonoBehaviour
+{
+    public AFX_Single ambience;
+}
